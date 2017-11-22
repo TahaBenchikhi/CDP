bc162acbae4b201d87ce7ea4846716e130a45985
Benchikhi Taha <benchikhi.taha@gmail.com>
2017-11-22 10:47:08 +0100

    Sprint2.2

--- Sprints/Sprint2/Sprint2Code/Serveur/Routes/index.js
+++ Sprints/Sprint2/Sprint2Code/Serveur/Routes/index.js
@@ -3,6 +3,8 @@ module.exports = function(app){
     require('./login')(app);
     require('./projet')(app);
     require('./equipe')(app);
+    require('./backlog')(app);
+    require('./sprint')(app);
 
     
 }
